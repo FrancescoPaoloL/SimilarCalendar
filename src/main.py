051d52760cbb92381e1ca157954830b212982186
FrancescoPaoloL <francescopaololezza@gmail.com>
2024-02-03 21:14:51 +0100

    Implemented logic

---
+++ src/main.py
@@ -0,0 +1,33 @@
+def find_similar_years(base_year, end_year):
+    similar_years = []
+    remainder = base_year % 4
+
+    if remainder == 0:
+        repeat_year = base_year + 28
+    elif remainder == 1:
+        repeat_year = base_year + 6
+    else:
+        repeat_year = base_year + 11
+
+    while repeat_year <= end_year:
+        similar_years.append(repeat_year)
+        
+        if (repeat_year % 4 == 0 and repeat_year % 100 != 0) or repeat_year % 400 == 0:
+            repeat_year += 28
+        else:
+            remainder = repeat_year % 4
+            if remainder == 0:
+                repeat_year += 28
+            elif remainder == 1:
+                repeat_year += 6
+            else:
+                repeat_year += 11
+
+    return similar_years
+
+
+
+base_year = int(input("Enter the base year: "))
+similar_years = find_similar_years(base_year, 2100)
+
+print(f"The years similar to {base_year} are: {', '.join(map(str, similar_years))}")
